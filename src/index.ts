951e0096dbe160bdc9c9f642e05f1c1947634f37
Dz-devz <darwinbjordan@gmail.com>
2024-07-16 19:39:47 +0800

    progress: added cookies route and setCookie

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Hono } from 'hono'
+import {setCookie} from "hono/dist/types/helper/cookie"
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+}).get("/set-cookie", (c) => {
+  setCookie(c,
+    "cookie_name",
+    "coo",
+    {httpOnly: true}
+  )
+  return c.text("done")
 })
 
 export default app
