487708acae1e48a67da597902363b20065cde175
Dz-devz <darwinbjordan@gmail.com>
2024-07-17 20:30:31 +0800

    blockers: added session but still have an error

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import {setCookie} from "hono/cookie"
 import { MiddleWare } from '../middleware/middleware'
 
+
 const app = new Hono()
 
 MiddleWare();
@@ -10,6 +11,13 @@ app.get('/', (c) => {
   console.log("secret: " + Bun.env.SESSION_ENCRYPTION_KEY);
   
   return c.text('Hello Hono!')
+}).get("/login", (c) => {
+    const session = c.get('session')
+    console.log("cookie" + JSON.stringify(c.get('session')))
+
+    session.set("userId", 85466754976)
+    
+    return c.redirect("/")
 }).get("/set-cookie", (c) => {
   setCookie(c,
     "cookie_name",
