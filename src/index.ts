89a8c255b296b57872b692c10b611c861922f330
Dz.dev <darwinbjordan@gmail.com>
2024-07-27 18:14:50 +0800

    fix: not getting session but have types error the fix is to get the session is to put the middleware instead of making another folder for it

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,40 @@
-import { Hono } from 'hono'
-import {setCookie} from "hono/cookie"
-import { MiddleWare } from '../middleware/middleware'
+import { Hono } from "hono";
+import { CookieStore, sessionMiddleware } from "hono-sessions";
+import { setCookie } from "hono/cookie";
 
+const app = new Hono();
+const store = new CookieStore();
 
-const app = new Hono()
+app.use(
+  "*",
+  sessionMiddleware({
+    store,
+    encryptionKey: Bun.env.SESSION_ENCRYPTION_KEY,
+    expireAfterSeconds: 900,
+    cookieOptions: {
+      path: "/",
+      httpOnly: true,
+    },
+  })
+);
 
-MiddleWare();
+app
+  .get("/", (c) => {
+    console.log("secret: " + Bun.env.SESSION_ENCRYPTION_KEY);
 
-app.get('/', (c) => {
-  console.log("secret: " + Bun.env.SESSION_ENCRYPTION_KEY);
-  
-  return c.text('Hello Hono!')
-}).get("/login", (c) => {
-    const session = c.get('session')
-    console.log("cookie" + JSON.stringify(c.get('session')))
+    return c.text("Hello Hono!");
+  })
+  .get("/login", (c) => {
+    const session = c.get("session");
+    console.log("cookie" + JSON.stringify(c.get("session")));
 
-    session.set("userId", 85466754976)
-    
-    return c.redirect("/")
-}).get("/set-cookie", (c) => {
-  setCookie(c,
-    "cookie_name",
-    "cookie_value",
-    {httpOnly: true}
-  )
-  return c.text("done")
-})
+    session.set("userId", 85466754976);
 
-export default app
+    return c.redirect("/");
+  })
+  .get("/set-cookie", (c) => {
+    setCookie(c, "cookie_name", "cookie_value", { httpOnly: true });
+    return c.text("done");
+  });
+
+export default app;
